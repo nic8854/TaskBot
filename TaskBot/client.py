63409099324286ba2c2c47de0b2cbca6e63ec3bb
nic8854 <40262536+nic8854@users.noreply.github.com>
2024-12-28 14:09:49 +0100

    GET and DELETE now work, PUT needs work and CREATE hasnt been implemented

--- TaskBot/client.py
+++ TaskBot/client.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import os
+import time
 
+clear = lambda: os.system('cls')
 
 def make_request(url, method='GET', params=None, data=None, headers=None, echo=0):
     """
@@ -40,7 +43,8 @@ def make_request(url, method='GET', params=None, data=None, headers=None, echo=0
 
 base_url = 'http://localhost:8000/tasks'
 
-print("Choose Task from List using number or enter 0 to create")
+
+
 response = make_request(base_url)
 #print("response:")
 #print(response)
@@ -54,18 +58,65 @@ if response:
         print(f"{iterator+1} = {name_array[iterator]}")
         iterator += 1
 
-task_number = input()
+task_number = input("Choose Task from List using number or enter 0 to CREATE: ")
 task_number = int(task_number) - 1
 if(name_array[task_number] is not None and name_array[task_number] != ""):
-    print(f"You chose: {name_array[task_number]}")
+    print(f"\nYou chose: {name_array[task_number]}")
+    chosen_task_name = name_array[task_number]
 else:
     print("ERROR")
+
+print("GET request executing...\n")
+
+chosen_task_response = make_request(base_url, params={'name': chosen_task_name})
+clear()
+print(f"Data for {name_array[task_number]}")
+
+print(json.dumps(chosen_task_response, indent=2))
+
+crud_input = input('0 = leave\n1 = PUT\n2 = DELETE\nWould you like to perform an action on this task?: ')
+match crud_input:
+    case "0":
+        print("You have chosen to leave")
+        exit()
+    case "1":
+        time.sleep(1)
+        clear()
+
+        print("You have chosen PUT")
+        print(f"Data for {name_array[task_number]}")
+        print(json.dumps(chosen_task_response, indent=2))
+
+        if chosen_task_response:
+            param_iterator = 0
+            param_array = []
+            for item in chosen_task_response:
+                param_array.append(item)
+                print(f"{param_iterator+1} = {param_array[param_iterator]}")
+                param_iterator += 1
+
+        param_number = input("Choose which parameter to change: ")
+        param_number = int(param_number) - 1
+        param_name = param_array[param_number]
+
+        param_value = input("Enter value: ")
+
+        make_request(base_url, method='PUT', params={'name': chosen_task_name}, data={"operation" : "put"}, echo=1)
+    case "2":
+        print("You have chosen DELETE")
+        make_request(base_url, method='DELETE', params={'name': chosen_task_name})
+    case _:
+        print("ERROR")
+
+time.sleep(1)
+clear()
+
 
 """
 # Step 1: Create two tasks
 print("STEP 1: Create Task 1")
 task_1 = {
-    "name": "Task1",
+    "name": "Task3",
     "operation": "get",
     "destination": "http://example.com/task1",
     "Parameters": {
@@ -78,7 +129,7 @@ make_request(base_url, method='POST', data=task_1)
 
 print("STEP 1: Create Task 2")
 task_2 = {
-    "name": "Task2",
+    "name": "Task4",
     "operation": "post",
     "destination": "http://example.com/task2",
     "Parameters": {
@@ -88,6 +139,7 @@ task_2 = {
     }
 }
 make_request(base_url, method='POST', data=task_2)
+
 
 # Step 2: Read all tasks
 print("STEP 2: Read all tasks")
